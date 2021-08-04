42a8f26811a3e3d26fe086380cd2e14aee1c0fde
Ross Parker <rosshparker@gmail.com>
2021-08-04 16:33:42 -0400

    updated I clustering again

--- scripts/Iclusters2.m
+++ scripts/Iclusters2.m
@@ -10,7 +10,7 @@ Nc  = 1;
 Nci = 20;
 
 p  = round( Ne/Nc );
-pi = round( Ni/Nci );
+pinh = round( Ni/Nci );
 
 mm = 0.7;
 mee = mm;
@@ -18,36 +18,27 @@ mie = mm;
 mei = -a*mm;
 mii = -a*mm;
 
-H = clusterEI(Nc, p, Nci, pi, mee, mei, mie, mii);
+H = clusterEI(Nc, p, Nci, pinh, mee, mei, mie, mii);
 l  = eig(H);
 realvals = uniquetol(real(l));
 
 gstar = sqrt(N)/realvals(end);
-g = gstar*1.05;
+g = gstar*1.02;
 
 l3 = (g/sqrt(N))*l-1;
 
 %%
-% timestepping
+% timesteppinhng
 
 x0 = randn(N,1)*0.5;
 
 fn = @(t, x)(-x + (H/sqrt(N))*tanh(g*x));
 
-t = linspace(0,200,2000);
-x = rk4(fn, x0, t);
+[t,x] = ode45(fn, [0,500], x0);
 
-J = Hx( H, g, x(:,end) );
+J = Hx( H, g, x(end,:)' );
 lJ = eig(J);
 realvalsJ = uniquetol(real(lJ));
-
-xexc = abs( x(1,end) );
-kexc = 1 - (gstar*xexc)^2;
-xinh = abs( x(end,end) );
-kinh = 1 - (gstar*xinh)^2;
-
-lc = (g/sqrt(N))*(p-1)*kexc*mee - 1;
-li = (g/sqrt(N))*a*mee - 1;
 
 figure;
 subplot(2,2,1);
@@ -55,7 +46,7 @@ plot(real(l), imag(l), '.r', 'MarkerSize', 30);
 subplot(2,2,2);
 plot(real(l3), imag(l3), '.', 'MarkerSize', 30);
 subplot(2,2,3);
-plot(t, x(1,:),'--b',t,x(f*N+1,:),'-r','LineWidth',1);
+plot(t, x(:,1),'--b',t,x(:,f*N+1),'-r','LineWidth',1);
 subplot(2,2,4);
 l4 = g*lJ/sqrt(N) - 1;
 plot(real(l4),imag(l4), '.', 'MarkerSize', 30);
@@ -68,8 +59,8 @@ set(groot,'defaultAxesTickLabelInterpreter','latex');
 set(groot,'defaulttextinterpreter','latex');
 set(groot,'defaultLegendInterpreter','latex');
 
-plot(t-150, x(1,:),'--b',t-150,x(f*N+1,:),'-r','LineWidth',3);
-axis([0 20 -7 7 ]);
+plot(t-400, x(:,1),'--b',t-400,x(:,f*N+1),'-r','LineWidth',3);
+axis([0 20 -5 5 ]);
 labels = {'$x_E$','$x_I$'};
 legend(labels,'Location','NorthEast');
 xlabel('$t$');
@@ -81,12 +72,12 @@ set(gca,'fontname','times');
 set(groot,'defaultAxesTickLabelInterpreter','latex');  
 set(groot,'defaulttextinterpreter','latex');
 set(groot,'defaultLegendInterpreter','latex');
-plot(x(1,700:end),x(f*N+1,700:end),'LineWidth',3);
+plot(x(3500:end,1),x(3500:end,f*N+1),'LineWidth',3);
 xlabel('$x_E$');
 ylabel('$x_I$');
 
-[pks,locs] = findpeaks(x(1,:));
-per = t(locs(end)) - t(locs(end-1));
+[pks,locs] = findpeaks(x(:,1));
+per = mean( t(locs(end-20:end)) - t(locs(end-21:end-1)) );
 
 
 %% functions
@@ -106,11 +97,11 @@ function H = cluster(N, Nc, p, mee, mei, mie, mii)
 end
 
 % allow excitatory and inhibitory clusters
-function H = clusterEI(Nc, p, Nci, pi, mee, mei, mie, mii)
-    ni = Nci*pi;
+function H = clusterEI(Nc, p, Nci, pinh, mee, mei, mie, mii)
+    ni = Nci*pinh;
     ne = Nc*p;
     eblock = mee*clusterblock(Nc, p);        
-    iblock = mii*clusterblock(Nci, pi);     
+    iblock = mii*clusterblock(Nci, pinh);     
     eiblock = mei*ones( ne, ni);
     ieblock = mie*ones( ni, ne);  
     H = [ eblock eiblock ; ieblock iblock ];
