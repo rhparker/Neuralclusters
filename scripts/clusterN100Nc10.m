099bed8ec462cc7fe4eea911ddae499d85641ef2
Ross Parker <rosshparker@gmail.com>
2021-09-06 20:49:20 -0500

    added Nc10 plot

---
+++ scripts/clusterN100Nc10.m
@@ -0,0 +1,253 @@
+% parameters for simulation
+
+N = 100;
+nI = 20;
+nE = 80;
+nC = 10;
+p = 8;
+alpha = 4;
+f = 0.8;
+mee = 0.7;
+
+%% plot of all branches
+
+% default MATLAB colors
+c1 = [0 0.4470 0.7410];
+c2 = [0.8500 0.3250 0.0980];
+c3 = [0.9290 0.6940 0.1250];
+c4 = [0.4940 0.1840 0.5560];
+c5 = [0.4660, 0.6740, 0.1880];          	
+c6 = [0.3010, 0.7450, 0.9330];	          	
+c7 = [0.6350, 0.0780, 0.1840];
+
+% plot parameters
+fontSize = 20;
+labelFontSize = 20;
+markerSize = 50;
+lw = 3;
+
+% when loading the data, we flip the excitation values to be consistent 
+% with the notation in the manuscript
+
+load EclusterN100Nc10beta1;
+x1 = -br{1}.xS;
+g1 = br{1}.parS;
+x1s = -sb{1}.xS;
+g1s = sb{1}.parS;
+BP1 = BP(2); 
+BP1index = find( abs(g1-BP1) < 1e-4);
+BP1index = BP1index(1);
+
+load EclusterN100Nc10beta2;
+x2 = -br{1}.xS;
+g2 = br{1}.parS;
+x2s = -sb{1}.xS;
+g2s = sb{1}.parS;
+BP2 = BP(3); 
+BP2index = find( abs(g2-BP2) < 1e-4);
+
+load EclusterN100Nc10beta3;
+x3 = -br{1}.xS;
+g3 = br{1}.parS;
+x3s = -sb{1}.xS;
+g3s = sb{1}.parS;
+BP3 = BP(2); 
+BP3index = find( abs(g3-BP3) < 1e-4);
+
+load EclusterN100Nc10beta4;
+x4 = -br{1}.xS;
+g4 = br{1}.parS;
+x4s = -sb{1}.xS;
+g4s = sb{1}.parS;
+BP4 = HB; 
+BP4index = find( abs(g4-BP4(2)) < 1e-4);
+BP4index2 = find( abs(g4-BP4(1)) < 1e-4);
+
+load EclusterN100Nc10beta5;
+x5 = -br{1}.xS;
+g5 = br{1}.parS;
+x5s = -sb{1}.xS;
+g5s = sb{1}.parS;
+BP5 = HB; 
+BP5index = find( abs(g5-BP5(2)) < 1e-4);
+BP5index2 = find( abs(g5-BP5(1)) < 1e-4);
+
+%%
+
+% excitatory, xE1 and xE2
+
+figure('DefaultAxesFontSize',fontSize);
+set(gca,'fontname','times');
+set(groot,'defaultAxesTickLabelInterpreter','latex');  
+set(groot,'defaulttextinterpreter','latex');
+set(groot,'defaultLegendInterpreter','latex');
+
+hold on;
+% zero solution
+plot([0 g1(1)], [0 0] ,'-','LineWidth',lw, 'Color', c1 );
+plot([g1(1) 20], [0 0] ,'--','LineWidth',lw, 'Color', c1 );
+% beta=1
+plot(g1,x1(:,1),'--', 'LineWidth', lw, 'Color', c2 );
+plot(g1,x1(:,10),'--', 'LineWidth', lw, 'Color', c2 );
+plot(g1s,x1s(:,1),'-', 'LineWidth', lw, 'Color', c2 );
+plot(g1s,x1s(:,10),'-', 'LineWidth', lw, 'Color', c2 );
+% beta=1.5
+plot(g2,x2(:,1),'--', 'LineWidth', lw, 'Color', c3 );
+plot(g2,x2(:,10),'--', 'LineWidth', lw, 'Color', c3 );
+plot(g2s,x2s(:,1),'-', 'LineWidth', lw, 'Color', c3 );
+plot(g2s,x2s(:,10),'-', 'LineWidth', lw, 'Color', c3 );
+% beta=7/3
+plot(g3,x3(:,1),'--', 'LineWidth', lw, 'Color', c4 );
+plot(g3,x3(:,10),'--', 'LineWidth', lw, 'Color', c4 );
+plot(g3s,x3s(:,1),'-', 'LineWidth', lw, 'Color', c4 );
+plot(g3s,x3s(:,10),'-', 'LineWidth', lw, 'Color', c4 );
+% beta=4
+plot(g4,x4(:,1),'--', 'LineWidth', lw, 'Color', c5 );
+plot(g4,x4(:,10),'--', 'LineWidth', lw, 'Color', c5 );
+plot(g4s,x4s(:,1),'-', 'LineWidth', lw, 'Color', c5 );
+plot(g4s,x4s(:,10),'-', 'LineWidth', lw, 'Color', c5 );
+% beta=9
+plot(g5,x5(:,1),'--', 'LineWidth', lw, 'Color', c6 );
+plot(g5,x5(:,10),'--', 'LineWidth', lw, 'Color', c6 );
+plot(g5s,x5s(:,1),'-', 'LineWidth', lw, 'Color', c6 );
+plot(g5s,x5s(:,10),'-', 'LineWidth', lw, 'Color', c6 );
+% branch points
+plot( [g1(1)], [0], '.k', 'MarkerSize', 40); 
+plot( [BP1 BP1], x1(BP1index, [1 10]), '.k', 'MarkerSize', 40);  
+plot( [BP2 BP2], x2(BP2index, [1 10]), '.k', 'MarkerSize', 40); 
+plot( [BP3 BP3], x3(BP3index, [1 10]), '.k', 'MarkerSize', 40); 
+plot( [BP4(2) BP4(2)], x4(BP4index, [1 10]), '.k', 'MarkerSize', 40); 
+plot( [BP5(2) BP5(2)], x5(BP5index, [1 10]), '.k', 'MarkerSize', 40); 
+
+axis([0,12,-10,7]);
+xlabel('$g$');
+ylabel('$X_E$');
+text(0.2,6,'$X_{E_1}$','FontSize',labelFontSize,'interpreter','latex');
+text(0.2,-9,'$X_{E_2}$','FontSize',labelFontSize,'interpreter','latex');
+
+
+%%
+
+% excitatory, xE1 only
+
+figure('DefaultAxesFontSize',fontSize);
+set(gca,'fontname','times');
+set(groot,'defaultAxesTickLabelInterpreter','latex');  
+set(groot,'defaulttextinterpreter','latex');
+set(groot,'defaultLegendInterpreter','latex');
+
+hold on;
+% zero solution
+plot([0 g1(1)], [0 0] ,'-','LineWidth',lw, 'Color', c1 );
+plot([g1(1) 20], [0 0] ,'--','LineWidth',lw, 'Color', c1 );
+% beta=1
+plot(g1,x1(:,1),'--', 'LineWidth', lw, 'Color', c2 );
+plot(g1s,x1s(:,1),'-', 'LineWidth', lw, 'Color', c2 );
+% beta=1.5
+plot(g2,x2(:,1),'--', 'LineWidth', lw, 'Color', c3 );
+plot(g2s,x2s(:,1),'-', 'LineWidth', lw, 'Color', c3 );
+% beta=7/3
+plot(g3,x3(:,1),'--', 'LineWidth', lw, 'Color', c4 );
+plot(g3s,x3s(:,1),'-', 'LineWidth', lw, 'Color', c4 );
+% beta=4
+plot(g4,x4(:,1),'--', 'LineWidth', lw, 'Color', c5 );
+plot(g4s,x4s(:,1),'-', 'LineWidth', lw, 'Color', c5 );
+% beta=9
+plot(g5,x5(:,1),'--', 'LineWidth', lw, 'Color', c6 );
+plot(g5s,x5s(:,1),'-', 'LineWidth', lw, 'Color', c6 );
+% branch points
+plot( [g1(1)], [0], '.k', 'MarkerSize', 40); 
+plot( [BP1 BP1], x1(BP1index, [1]), '.k', 'MarkerSize', 40);  
+plot( [BP2 BP2], x2(BP2index, [1]), '.k', 'MarkerSize', 40); 
+plot( [BP3 BP3], x3(BP3index, [1]), '.k', 'MarkerSize', 40); 
+plot( [BP4(2) BP4(2)], x4(BP4index, [1]), '.k', 'MarkerSize', 40); 
+plot( [BP5(2) BP5(2)], x5(BP5index, [1]), '.k', 'MarkerSize', 40); 
+% stability switch points
+plot( [BP4(1)], x4(BP4index2, [1]), 'dk', 'MarkerSize', 10, 'LineWidth',3); 
+plot( [BP5(1)], x5(BP5index2, [1]), 'dk', 'MarkerSize', 10, 'LineWidth',3);
+
+xlabel('$g$');
+ylabel('$X_{E_1}$');
+
+% % version 1, zoomed out
+% axis([0,12,-0.2,5.5]);
+% gpos = [ 8 ; 8.5 ; 8.5 ; 8 ; 8 ];
+% Elabelpos = [ 5 ; 3.85 ; 2.7 ; 1.55 ; 0.45];
+% labels = {'$\beta_C = 1$','$\beta_C = 3/2$', '$\beta_C = 7/3$', '$\beta_C = 4$', '$\beta_C = 9$'};
+% labelpoints( gpos, Elabelpos, labels,'NW',0,0,'FontSize',labelFontSize,'interpreter','latex');
+
+% version 2, zoomed in
+axis([0,2,-0.2,5.5]);
+gpos = [ 1 ; 1.1 ; 1.1 ; 1 ; 1 ];
+Elabelpos = [ 5 ; 4 ; 3 ; 2 ; 0.9];
+labels = {'$\beta_C = 1$','$\beta_C = 3/2$', '$\beta_C = 7/3$', '$\beta_C = 4$', '$\beta_C = 9$'};
+labelpoints( gpos, Elabelpos, labels,'NW',0,0,'FontSize',labelFontSize,'interpreter','latex');
+
+%% inhibitory
+
+figure('DefaultAxesFontSize',fontSize);
+set(gca,'fontname','times');
+set(groot,'defaultAxesTickLabelInterpreter','latex');  
+set(groot,'defaulttextinterpreter','latex');
+set(groot,'defaultLegendInterpreter','latex');
+
+hold on;
+% zero solution
+plot([0 g1(1)], [0 0] ,'-','LineWidth',lw, 'Color', c1 );
+plot([g1(1) 20], [0 0] ,'--','LineWidth',lw, 'Color', c1 );
+% beta=1
+plot(g1,x1(:,11),'--', 'LineWidth', lw, 'Color', c2 );
+plot(g1s,x1s(:,11),'-', 'LineWidth', lw, 'Color', c2 );
+% beta=1.5
+plot(g2,x2(:,11),'--', 'LineWidth', lw, 'Color', c3 );
+plot(g2s,x2s(:,11),'-', 'LineWidth', lw, 'Color', c3 );
+% beta=7/3
+plot(g3,x3(:,11),'--', 'LineWidth', lw, 'Color', c4 );
+plot(g3s,x3s(:,11),'-', 'LineWidth', lw, 'Color', c4 );
+% beta=4
+plot(g4,x4(:,11),'--', 'LineWidth', lw, 'Color', c5 );
+plot(g4s,x4s(:,11),'-', 'LineWidth', lw, 'Color', c5 );
+% beta=9
+plot(g5,x5(:,11),'--', 'LineWidth', lw, 'Color', c6 );
+plot(g5s,x5s(:,11),'-', 'LineWidth', lw, 'Color', c6 );
+% branch points
+plot( [g1(1)], [0], '.k', 'MarkerSize', 40); 
+plot( [BP1], x1(BP1index, [11]), '.k', 'MarkerSize', 40);  
+plot( [BP2], x2(BP2index, [11]), '.k', 'MarkerSize', 40); 
+plot( [BP3], x3(BP3index, [11]), '.k', 'MarkerSize', 40); 
+plot( [BP4(2)], x4(BP4index, [11]), '.k', 'MarkerSize', 40); 
+plot( [BP5(2)], x5(BP5index, [11]), '.k', 'MarkerSize', 40); 
+% stability switch points
+plot( [BP4(1)], x4(BP4index2, [11]), 'dk', 'MarkerSize', 10, 'LineWidth',3); 
+plot( [BP5(1)], x5(BP5index2, [11]), 'dk', 'MarkerSize', 10, 'LineWidth',3);
+
+xlabel('$g$');
+ylabel('$X_I$');
+
+% % version 1, zoomed out
+% axis([0,12,-0.1,0.7]);
+% gpos = [ 2.2 ; 2.65 ; 5.25 ; 2.6 ; 6.2 ];
+% Ilabelpos = [ -0.06 ; 0.025 ; 0.4 ; 0.6 ; 0.22 ];
+% labels = {'$\beta_C = 1$','$\beta_C = 3/2$', '$\beta_C = 7/3$', '$\beta_C = 4$', '$\beta_C = 9$'};
+% labelpoints( gpos, Ilabelpos, labels,'NW',0,0,'FontSize',labelFontSize,'interpreter','latex');
+% plot([2 3.1],[0.22 0.4],'-k','Linewidth',2);
+
+% version 2, zoomed in
+axis([0,2,-0.1,0.7]);
+gpos = [ 1 ; 1.15 ; 1.9 ; 1.1 ; 1.9 ];
+Ilabelpos = [ -0.07 ; 0.10 ; 0.25 ; 0.6 ; 0.42 ];
+labels = {'$\beta_C = 1$','$\beta_C = 3/2$', '$\beta_C = 7/3$', '$\beta_C = 4$', '$\beta_C = 9$'};
+labelpoints( gpos, Ilabelpos, labels,'NW',0,0,'FontSize',labelFontSize,'interpreter','latex');
+
+%%
+
+% gpos = [ g1(140) ; g15(140); g233(140); g4(120); g9(100) ];
+% Elabelpos = [ x1(140,1) ; x15(140,1); x233(140,1); x4(120,1); x9(100,1) ];
+% labels = {'$\beta = 1$','$\beta = 3/2$', '$\beta = 7/3$', '$\beta = 4$', '$\beta = 9$'};
+% 
+% 
+% labelpoints( gpos, Elabelpos, labels,'NW',0,0,'FontSize',labelFontSize,'interpreter','latex');
+% 
+% xlabel('$g$');
+% ylabel('$X_E$');
+
---
+++ scripts/clusterN75Nc10.m
@@ -0,0 +1,203 @@
+% parameters for simulation
+
+N = 75;
+nI = 15;
+nE = 60;
+nC = 10;
+p = 6;
+alpha = 4;
+f = 0.8;
+mee = 0.7;
+
+%% plot of all branches
+
+% default MATLAB colors
+c1 = [0 0.4470 0.7410];
+c2 = [0.8500 0.3250 0.0980];
+c3 = [0.9290 0.6940 0.1250];
+c4 = [0.4940 0.1840 0.5560];
+c5 = [0.4660, 0.6740, 0.1880];          	
+c6 = [0.3010, 0.7450, 0.9330];	          	
+c7 = [0.6350, 0.0780, 0.1840];
+
+% plot parameters
+fontSize = 20;
+labelFontSize = 20;
+markerSize = 50;
+lw = 3;
+
+load EclusterN75Nc4beta1;
+x1 = br{1}.xS;
+g1 = br{1}.parS;
+x1s = sb{1}.xS;
+g1s = sb{1}.parS;
+BP1 = BP; 
+BP1index = find( abs(g1-BP1) < 1e-4);
+BP1index = BP1index(1);
+
+load EclusterN75Nc4beta2;
+x2 = br{1}.xS;
+g2 = br{1}.parS;
+x2s = sb{1}.xS;
+g2s = sb{1}.parS;
+BP2 = BP; 
+BP2index = find( abs(g2-BP2) < 1e-4);
+
+load EclusterN75Nc4beta3;
+x3 = br{1}.xS;
+g3 = br{1}.parS;
+x3s = sb{1}.xS;
+g3s = sb{1}.parS;
+BP3 = BP; 
+BP3index = find( abs(g3-BP3) < 1e-4);
+
+load EclusterN75Nc4beta4;
+x4 = br{1}.xS;
+g4 = br{1}.parS;
+x4s = sb{1}.xS;
+g4s = sb{1}.parS;
+BP4 = BP; 
+BP4index = find( abs(g4-BP4(2)) < 1e-4);
+
+load EclusterN75Nc4beta5;
+x5 = br{1}.xS;
+g5 = br{1}.parS;
+BP5 = BP; 
+BP5index = find( abs(g5-BP5) < 1e-4);
+
+%%
+
+% excitatory, xE1 and xE2
+
+figure('DefaultAxesFontSize',fontSize);
+set(gca,'fontname','times');
+set(groot,'defaultAxesTickLabelInterpreter','latex');  
+set(groot,'defaulttextinterpreter','latex');
+set(groot,'defaultLegendInterpreter','latex');
+
+hold on;
+% zero solution
+plot([0 g1(1)], [0 0] ,'-','LineWidth',lw, 'Color', c1 );
+% beta=1
+plot(g1,x1(:,1),'--', 'LineWidth', lw, 'Color', c2 );
+plot(g1,x1(:,10),'--', 'LineWidth', lw, 'Color', c2 );
+plot(g1s,x1s(:,1),'-', 'LineWidth', lw, 'Color', c2 );
+plot(g1s,x1s(:,10),'-', 'LineWidth', lw, 'Color', c2 );
+% beta=1.5
+plot(g2,x2(:,1),'--', 'LineWidth', lw, 'Color', c3 );
+plot(g2,x2(:,10),'--', 'LineWidth', lw, 'Color', c3 );
+plot(g2s,x2s(:,1),'-', 'LineWidth', lw, 'Color', c3 );
+plot(g2s,x2s(:,10),'-', 'LineWidth', lw, 'Color', c3 );
+% beta=7/3
+plot(g3,x3(:,1),'--', 'LineWidth', lw, 'Color', c4 );
+plot(g3,x3(:,10),'--', 'LineWidth', lw, 'Color', c4 );
+plot(g3s,x3s(:,1),'-', 'LineWidth', lw, 'Color', c4 );
+plot(g3s,x3s(:,10),'-', 'LineWidth', lw, 'Color', c4 );
+% beta=4
+plot(g4,x4(:,1),'--', 'LineWidth', lw, 'Color', c5 );
+plot(g4,x4(:,10),'--', 'LineWidth', lw, 'Color', c5 );
+% plot(g4s,x4s(:,1),'-', 'LineWidth', lw, 'Color', c3 );
+% plot(g4s,x4s(:,10),'-', 'LineWidth', lw, 'Color', c3 );
+% beta=9
+plot(g5,x5(:,1),'--', 'LineWidth', lw, 'Color', c6 );
+plot(g5,x5(:,10),'--', 'LineWidth', lw, 'Color', c6 );
+axis([0,4,-4,6]);
+% branch points
+plot( [BP1(2) BP1(2)], x1(BP1index, [1 10]), '.k', 'MarkerSize', 40);  
+plot( [BP2(2) BP2(2)], x2(BP1index, [1 10]), '.k', 'MarkerSize', 40); 
+plot( [BP3(1) BP3(1)], x3(BP1index, [1 10]), '.k', 'MarkerSize', 40); 
+plot( [BP4(1) BP4(1)], x4(BP1index, [1 10]), '.k', 'MarkerSize', 40); 
+
+xlabel('$g$');
+ylabel('$X_I$');
+text(0.1,4.5,'$X_{E_1}$','FontSize',labelFontSize,'interpreter','latex');
+text(0.1,-3,'$X_{E_2}$','FontSize',labelFontSize,'interpreter','latex');
+
+%%
+
+plot( g0, 0, '.k', 'MarkerSize', 40);                   % branch points
+
+
+
+%% inhibitory, only xI2
+
+gpos = [ g1(70) ; g15(80); g233(80); g4(80); g9(80) ];
+I2labelpos = [ x1(70,2) ; x15(80,2)+0.01; x233(80,2)+0.01; x4(70,2)+0.0175; x9(70,2)+0.0175 ];
+labels = {'$\beta = 1$','$\beta = 3/2$', '$\beta = 7/3$', '$\beta = 4$', '$\beta = 9$'};
+
+figure('DefaultAxesFontSize',fontSize);
+set(gca,'fontname','times');
+set(groot,'defaultAxesTickLabelInterpreter','latex');  
+set(groot,'defaulttextinterpreter','latex');
+set(groot,'defaultLegendInterpreter','latex');
+
+hold on;
+% zero solution
+plot(g00,x0(:,1),'--', 'LineWidth', lw, 'Color', c1 );
+plot(g0s,g0x(:,1),'-','LineWidth',lw, 'Color', c1 );
+% beta=1
+plot(g1,x1(:,2),'--', 'LineWidth', lw, 'Color', c2 );
+plot(g1s,g1x(:,2),'-', 'LineWidth', lw, 'Color', c2 );
+% beta=1.5
+plot(g15,x15(:,2),'--', 'LineWidth', lw, 'Color', c3 );
+plot(g15s,g15x(:,2),'-', 'LineWidth', lw, 'Color', c3 );
+% beta=7/3
+plot(g233,x233(:,2),'--', 'LineWidth', lw, 'Color', c4 );
+% beta=4
+plot(g4,x4(:,2),'--', 'LineWidth', lw, 'Color', c5 );
+% beta=9
+plot(g9,x9(:,2),'--', 'LineWidth', lw, 'Color', c6 );
+
+g0 = sqrt(50)/(4*0.7);
+plot( g0, 0, '.k', 'MarkerSize', 40);                   % branch points
+scatter( Hopfx, Hopfy(:,2), 175, 'sk', 'filled');       % Hopf (I2)
+
+axis([2.45,3.55,-0.34,0.01]);
+
+labelpoints( gpos, I2labelpos, labels,'S',0.5,1,'FontSize',labelFontSize,'interpreter','latex');
+% labelpoints( gpos(1), I2labelpos(1), labels(1),'NE',0.5,1,'FontSize',labelFontSize,'interpreter','latex');
+% labelpoints( gpos(2), I2labelpos(2), labels(2),'SE',0.5,1,'FontSize',labelFontSize,'interpreter','latex');
+
+xlabel('$g$');
+ylabel('$X_{I_2}$');
+
+%% Excitatory
+
+gpos = [ g1(140) ; g15(140); g233(140); g4(120); g9(100) ];
+Elabelpos = [ x1(140,1) ; x15(140,1); x233(140,1); x4(120,1); x9(100,1) ];
+labels = {'$\beta = 1$','$\beta = 3/2$', '$\beta = 7/3$', '$\beta = 4$', '$\beta = 9$'};
+
+figure('DefaultAxesFontSize',fontSize);
+set(gca,'fontname','times');
+set(groot,'defaultAxesTickLabelInterpreter','latex');  
+set(groot,'defaulttextinterpreter','latex');
+set(groot,'defaultLegendInterpreter','latex');
+
+hold on;
+% zero solution
+% plot(g00,x0(:,1),'--', 'LineWidth', lw, 'Color', c1 );
+plot(g0s,g0x(:,1),'-','LineWidth',lw, 'Color', c1 );
+% beta=1
+plot(g1,x1(:,1),'--', 'LineWidth', lw, 'Color', c2 );
+plot(g1s,g1x(:,1),'-', 'LineWidth', lw, 'Color', c2 );
+% beta=1.5
+plot(g15,x15(:,1),'--', 'LineWidth', lw, 'Color', c3 );
+plot(g15s,g15x(:,1),'-', 'LineWidth', lw, 'Color', c3 );
+% beta=7/3
+plot(g233,x233(:,1),'--', 'LineWidth', lw, 'Color', c4 );
+% beta=4
+plot(g4,x4(:,1),'--', 'LineWidth', lw, 'Color', c5 );
+% beta=9
+plot(g9,x9(:,1),'--', 'LineWidth', lw, 'Color', c6 );
+
+g0 = sqrt(50)/(4*0.7);
+plot( g0, 0, '.k', 'MarkerSize', 40);                   % branch points
+scatter( Hopfx, Hopfy(:,1), 175, 'sk', 'filled');       % Hopf (I2)
+
+axis([2.45,3.55,-0.002,0.03]);
+
+labelpoints( gpos, Elabelpos, labels,'NW',0,0,'FontSize',labelFontSize,'interpreter','latex');
+
+xlabel('$g$');
+ylabel('$X_E$');
+
